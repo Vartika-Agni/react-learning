12f8fac6d0857c4fc062bb5c1cf70bf77d8bbd23
Varika <vartikaagnihotri31@gmail.com>
2025-06-27 13:00:15 +0530

    Vite+React

---
+++ 01basicsvite/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <h1> vite + react</>
+      
+    </>
+  )
+}
+
+export default App
